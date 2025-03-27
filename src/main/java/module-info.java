82e2e0f349c37ae7da4ea0ff72f5d9cf61a92810
clairefielden <fieldenclaire@gmail.com>
2025-03-27 12:02:53 +0200

    Fixed "Caused by: org.springframework.cglib.core.CodeGenerationException: java.lang.IllegalAccessException-->module FirstWebapp does not open springproject.firstwebapp to unnamed module @b1bc7ed" error

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,8 +1,9 @@
-module FirstWebapp {
+open module FirstWebapp {
     requires jakarta.persistence;
     requires spring.boot;
     requires spring.boot.autoconfigure;
     requires spring.context;
     requires spring.data.commons;
     requires spring.web;
+    requires spring.boot.starter.thymeleaf;
 }
