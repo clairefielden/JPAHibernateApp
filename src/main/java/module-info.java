ace5bf060e3cc26887674bf4c0e3e659c6b80cd7
clairefielden <fieldenclaire@gmail.com>
2025-03-27 12:12:01 +0200

    Fixed URLs to display info by changing variables in HTML

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -6,4 +6,7 @@ open module FirstWebapp {
     requires spring.data.commons;
     requires spring.web;
     requires spring.boot.starter.thymeleaf;
+    requires spring.core;
+    requires spring.beans;
+    requires org.hibernate.orm.core;
 }
